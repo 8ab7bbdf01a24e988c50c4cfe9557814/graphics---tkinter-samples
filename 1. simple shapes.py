54f46b59f20eb59a32d32e8da573c1d26a6f4c7b
8ab7bbdf01a24e988c50c4cfe9557814 <jonas.lemmon@det.nsw.edu.au>
2025-06-26 19:08:32 +1000

    Update 1. simple shapes.py
    removed unnecessary comment

--- 1. simple shapes.py
+++ 1. simple shapes.py
@@ -8,7 +8,6 @@ root.title("Drawing with tk")
 canvas = tk.Canvas(root, width=500, height=500)
 canvas.pack()
 
-# Draw a circle at (200, 200) with a radius of 50
 canvas.create_oval(100,100,200,200)  #100,100 to 200,200 is the bounding box of the oval (ie a circle)
 canvas.create_rectangle(100,300,200,400)  #100,300 to 200,400 is the bounding box of the rectangle (ie a square)
 canvas.create_line(300,100,400,200) #draw a line from 300,100 to 400,200
